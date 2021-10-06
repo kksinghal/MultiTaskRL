Change bottleneck of resnet; Fix error of softmax in attn

Agent.py
@@ -10,10 +10,13 @@ from utils import *
 
 from pathlib import Path
 
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
+device1 = torch.device("cuda:0") # critic
+device2 = torch.device("cuda:1") # actor
 
-actor_learning_rate=1e-3
-critic_learning_rate=1e-3
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+actor_learning_rate=1e-4
+critic_learning_rate=1e-4
 gamma=0.99
 tau=1e-2
 max_buffer_size = 10000
@@ -23,17 +26,17 @@ class Agent():
 
         self.retention_time = retention_time
         
-        self.critic = critic(retention_time).to(device)
+        self.critic = critic(retention_time).to(device1)
         critic_file = Path("./parameters/critic")
         if critic_file.is_file():
             self.critic.load_state_dict(torch.load("./parameters/critic"))
-        self.critic_target = critic(retention_time).to(device)
+        self.critic_target = critic(retention_time).to(device1)
 
-        self.actor = actor(retention_time).to(device)
+        self.actor = actor(retention_time).to(device2)
         actor_file = Path("./parameters/actor")
         if actor_file.is_file():
             self.actor.load_state_dict(torch.load("./parameters/actor"))
-        self.actor_target = actor(retention_time).to(device)
+        self.actor_target = actor(retention_time).to(device2)
         
         for target_param, param in zip(self.actor_target.parameters(), self.actor.parameters()):
             target_param.data.copy_(param.data)

ResNet.py
@@ -1,6 +1,8 @@
+from numpy import NaN
 import torch
 from torch import nn
 import torchvision
+import sys
 
 from self_attention import self_attention
 
@@ -14,21 +16,15 @@ class bottle_neck(nn.Module):
 
         self.attn_1 = self_attention(in_channels, img_size, img_size, retention_time)
         self.batch_norm1 = nn.BatchNorm3d(in_channels)
-        self.attn_2 = self_attention(in_channels, img_size, img_size, retention_time)
-        self.batch_norm2 = nn.BatchNorm3d(in_channels)
-        self.relu = nn.ReLU()
 
         self.sequential = nn.Sequential(
             self.attn_1,
             self.batch_norm1,
-            self.relu,
-            self.attn_2,
-            self.batch_norm2,
         )
             
         
     def forward(self, X):
-        y = self.relu(self.sequential(X) + X)
+        y = self.sequential(X)
 
         return y
         
@@ -55,7 +51,7 @@ class downsample(nn.Module):
 
 
 class ResNet(nn.Module):
-    def __init__(self, in_channels, img_size, retention_time):
+    def __init__(self, img_size, retention_time):
         super(ResNet, self).__init__()
         
         self.stem = nn.Sequential(
@@ -72,9 +68,9 @@ class ResNet(nn.Module):
             self.blocks.append(self.make_layer(64*(2**i), int(img_size/(2**(i+1))), retention_time, n_blocks= 2))
 
 
-
     def forward(self, X):
         X = self.stem(X)
+
         shape = X.shape
         X = X.reshape(shape[0], -1, shape[-2], shape[-1])
         X = self.maxPool(X).reshape(*shape[:3], int(shape[3]/2), int(shape[4]/2))

actor.py
@@ -11,13 +11,9 @@ class actor(nn.Module):
         self.flatten = nn.Flatten()
         self.fc = nn.Sequential(
             nn.Linear(2048*3*2*2, 1024),
-            nn.ReLU(inplace=False),
-            nn.Linear(1024, 512),
-            nn.ReLU(inplace=False),
-            nn.Linear(512, 128),
-            nn.ReLU(inplace=False),
-            nn.Linear(128, 64),
-            nn.ReLU(inplace=False),
+            nn.Sigmoid(),
+            nn.Linear(1024, 64),
+            nn.Sigmoid(),
             nn.Linear(64, 3),
             nn.Tanh()
         )

critic.py
@@ -10,25 +10,17 @@ class critic(nn.Module):
         self.resnet = ResNet(in_channels=3, img_size=128, retention_time=retention_time)
         self.flatten = nn.Flatten()
         self.fc = nn.Sequential(
-            nn.Linear(2048*retention_time*2*2 + 3, 1024, bias=False),
-            nn.BatchNorm1d(1024),
-            nn.ReLU(),
-            nn.Linear(1024, 512, bias=False),
-            nn.BatchNorm1d(512),
-            nn.ReLU(),
-            nn.Linear(512, 128, bias=False),
-            nn.BatchNorm1d(128),
-            nn.ReLU(),
-            nn.Linear(128, 64, bias=False),
-            nn.BatchNorm1d(64),
-            nn.ReLU(),
+            nn.Linear(2048*retention_time*2*2 + 3, 1024),
+            nn.Sigmoid(),
+            nn.Linear(1024, 64),
+            nn.Sigmoid(),
             nn.Linear(64, 1)
         )
     
     def forward(self, X, actions):
         y = self.resnet(X)
-        y = self.flatten(y)
 
+        y = self.flatten(y)
         y = torch.cat((y, actions), dim=1)
         out = self.fc(y)
 

data_collection.py
@@ -1,139 +1,101 @@
-import torch
-from torchvision.utils import save_image
-
-import pandas as pd
-import numpy as np
-
 import os
+import sys
+import numpy as np
+import pandas as pd
+from Agent import Agent
+from utils import *
+import random
 import keyboard
-from pathlib import Path
-
+import torch
 
 from mlagents_envs.environment import UnityEnvironment
 from mlagents_envs.base_env import ActionTuple
 
 
-def calc_R(rewards, discount=0.95):
+env_path = None#"./Scenes/PushBlock_Win_slow/UnityEnvironment"
+env = UnityEnvironment(file_name=env_path,  seed=1, side_channels=[])
 
-    R = 0
-    batch_return = []
-    for reward in rewards[::-1]:
-        R = reward + discount*R
-        batch_return.append(R)
-    batch_return.reverse()
 
-    return batch_return
+retention_time = 3
+agent = Agent(retention_time)
 
+T_MAX = 300
+
+
+def get_input():
+    forward_force_mean = 0 
+    right_force_mean = 0 
+    angular_velocity_mean = 0
+    try:
+        if keyboard.is_pressed('w'):
+            forward_force_mean = 0.8
+        if keyboard.is_pressed('f'):
+            forward_force_mean = -0.8
+        if keyboard.is_pressed('s'):
+            forward_force_mean = -0.8
+        
+        if keyboard.is_pressed('d'):
+            right_force_mean = 0.8
+        if keyboard.is_pressed('a'):
+            right_force_mean = -0.8
+
+        if keyboard.is_pressed('m'):
+            angular_velocity_mean = 0.8
+        if keyboard.is_pressed('n'):
+            angular_velocity_mean = -0.8
+    except:
+        print("except")
+    return np.array([[forward_force_mean, right_force_mean, angular_velocity_mean]])*2
+
+buffer_data = pd.read_csv("./data/buffer_data.csv")
+
+total_episodes = 15
+for episode in range(total_episodes):
     
+    print("Episode: ", episode)
 
-N_GAMES = 5
-T_MAX=330
-env = UnityEnvironment(file_name=None,  seed=1, side_channels=[])
-
-task = "PushBlock"
-Path("./data/"+task).mkdir(parents=True, exist_ok=True)
-already_collected_episodes = len(next(os.walk('./data/'+task))[1])
-
-for episode_idx in range(already_collected_episodes, already_collected_episodes+N_GAMES):
-
-    forward_force_means= [] 
-    right_force_means= [] 
-    angular_velocity_means = []
-    rewards = []
-
-    os.mkdir("./data/"+task+"/"+str(episode_idx))
-    done = False
     env.reset()
     env.step()
-    behavior_names = env.behavior_specs.keys()
+
     behavior_name = list(env.behavior_specs)[0] 
-    score = 0
+    decision_steps, terminal_steps = env.get_steps(behavior_name)
 
-    t_step = 0
-    output = []
-    while not done:
+    state = torch.tensor(decision_steps.obs[0], dtype=torch.float64).squeeze().permute(2,0,1)
+    for t in range(retention_time):
+        agent.replay_buffer.push(state, None, None, None, None)
 
-        decision_steps, terminal_steps = env.get_steps(behavior_name)
+    done = False
+    for t_step in range(T_MAX):
+        if done:
+            break
 
-        observation = torch.Tensor(decision_steps.obs[0]).squeeze().permute(2,0,1)
+        action = get_input()
+        env_action = ActionTuple(action)
 
-        save_image(observation, './data/'+task+"/"+str(episode_idx)+'/'+ str(t_step) +'.png')
-
-        forward_force_mean = 0 
-        right_force_mean = 0 
-        angular_velocity_mean = 0
-        try:
-            if keyboard.is_pressed('w'):
-                forward_force_mean = 1
-            if keyboard.is_pressed('f'):
-                forward_force_mean = -1
-            if keyboard.is_pressed('s'):
-                forward_force_mean = -1
-            
-            if keyboard.is_pressed('d'):
-                right_force_mean = 1
-            if keyboard.is_pressed('a'):
-                right_force_mean = -1
-
-            if keyboard.is_pressed('m'):
-                angular_velocity_mean = 1
-            if keyboard.is_pressed('n'):
-                angular_velocity_mean = -1    
-            
-        except:
-            print("except")
-        
-        forward_force_means.append(forward_force_mean)
-        right_force_means.append(right_force_mean)
-        angular_velocity_means.append(angular_velocity_mean)
-
-        action = [forward_force_mean, right_force_mean, angular_velocity_mean]
-        env_action = ActionTuple(np.array([action]))
-
-        tmp_score = 0
+        reward = 0
         for i in range(3):
             if not done:
                 env.set_actions(behavior_name, env_action)
                 env.step()
                 decision_steps, terminal_steps = env.get_steps(behavior_name)
-                done = len(terminal_steps.reward)!=0 or t_step == T_MAX
+                done = len(decision_steps.reward)==0 or t_step == T_MAX
                 if not done:
-                    tmp_score += decision_steps.reward[0]
+                    reward += decision_steps.reward[0]
+                    new_state = torch.Tensor(decision_steps.obs[0]).squeeze().permute(2,0,1)
+
                 else:
-                    tmp_score += terminal_steps.reward[0]
+                    reward += terminal_steps.reward[0]
+                    new_state = torch.Tensor(terminal_steps.obs[0]).squeeze().permute(2,0,1)
 
-        score += tmp_score
-        rewards.append(tmp_score)
-        
+        agent.replay_buffer.push(state, torch.tensor(action), reward, new_state, done)
+        state = new_state
 
-        if done:
-            if t_step == T_MAX:
-                score -= 1
-            else:
-                score += terminal_steps.reward[0]
-            
-            returns = calc_R(rewards)
+        id = len(buffer_data)
+        torch.save(state, "./data/states/"+str(id)+".pt")
+        torch.save(new_state, "./data/next_states/"+str(id)+".pt")
 
-            df = pd.DataFrame({
-                "id": range(t_step+1),
-                "forward_force_mean": forward_force_means, 
-                "right_force_mean": right_force_means, 
-                "angular_velocity_mean": angular_velocity_means, 
-                "value": returns
-            })
+        data = pd.DataFrame([[id, *(action[0]), reward, done]], columns=buffer_data.columns)
+        buffer_data = buffer_data.append(data)
+        buffer_data.to_csv("./data/buffer_data.csv", index=False)
 
-            df.to_csv("./data/"+task+"/"+str(episode_idx)+"/"+"output.csv", index=False)
-
-            df2 = pd.DataFrame({
-                "task": [task],
-                "episode_idx": [episode_idx]
-            })
-            data = pd.read_csv("./data/metadata.csv", index_col=None)
-            data = data.append(df2)
-            data.to_csv("./data/metadata.csv", index=False)
-
-            
-
-        t_step += 1
-
-    print('episode ', episode_idx, 'reward %.1f' % score)
+        np.save("./data/sampling_ids", agent.replay_buffer.sampling_ids)

main.py
@@ -14,7 +14,7 @@ from mlagents_envs.base_env import ActionTuple
 from torch.utils.tensorboard import SummaryWriter
 writer = SummaryWriter('tensorboard/')
 
-env_path = "./Scenes/PushBlock_Win/UnityEnvironment"
+env_path = "./Scenes/PushBlock_Win_slow/UnityEnvironment"
 env = UnityEnvironment(file_name=env_path,  seed=1, side_channels=[])
 
 def set_seeds(seed):
@@ -61,8 +61,8 @@ def get_input():
         print("except")
     return np.array([[forward_force_mean, right_force_mean, angular_velocity_mean]])*2
 
-total_episodes = 10
-for episode in range(0,total_episodes, 3):
+total_episodes = 100
+for episode in range(36,total_episodes, 3):
 
     for i in range(3): #Collect 3 episodes
         print("Episode: ", episode+i)
@@ -85,13 +85,12 @@ for episode in range(0,total_episodes, 3):
             if done:
                 break
             
-            """
             if random.uniform(0, 1) < epsilon:
-                action = np.random.rand(1,3) * 2 - 2
+                action = np.random.rand(1,3) * 4 - 2
             else:    
                 action = agent.get_action()
-            """
-            action = get_input()
+            
+            #action = get_input()
             env_action = ActionTuple(action)
 
             reward = 0

self_attention.py
@@ -1,8 +1,6 @@
 import torch
 from torch import nn
 
-device = 'cpu'#cuda' if torch.cuda.is_available() else 'cpu'
-
 
 class self_attention(nn.Module):
 
@@ -27,15 +25,14 @@ class self_attention(nn.Module):
         self.task_value = torch.nn.Parameter(torch.rand((int(in_channels/2), retention_time, img_width, img_height)))
 
         self.positional_encoding = torch.nn.Parameter(torch.rand((in_channels, retention_time, img_height, img_width)))
-        
+
 
     def forward(self, X):
-
         query = (self.query_conv(X+self.positional_encoding) + self.task_query).reshape(-1, int(self.in_channels/2), self.img_width*self.img_height*self.retention_time).permute(0,2,1)
         key = (self.key_conv(X+self.positional_encoding) + self.task_key).reshape(-1, int(self.in_channels/2), self.img_width*self.img_height*self.retention_time)
 
         similarity = torch.matmul(query, key)
-        softmax = self.softmax(similarity)
+        similarity = self.softmax(similarity)
 
         value = (self.value_conv(X +self.positional_encoding) + self.task_value).reshape(-1, int(self.in_channels/2), self.img_width*self.img_height*self.retention_time).permute(0,2,1)
         

supervised_learning.py
@@ -1,86 +1,170 @@
-import os
-import torch
-from torch import nn
-from torch.utils.data import DataLoader, Dataset
-import torchvision
-from torchvision import datasets, transforms
-
+import sys
 import numpy as np
 import pandas as pd
-
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
-
+import matplotlib.pyplot as plt
 from Agent import Agent
-agent = Agent(n_heads=16).to(device)
+from utils import *
+import random
+
+import torch
+from torch.utils.data import DataLoader
+from torch.utils.data import Dataset
+from torchvision import transforms
+
+from mlagents_envs.environment import UnityEnvironment
+from mlagents_envs.base_env import ActionTuple
 
 from torch.utils.tensorboard import SummaryWriter
-writer = SummaryWriter('tensorboard/loss')
+writer = SummaryWriter('tensorboard/')
+
+def set_seeds(seed):
+    torch.manual_seed(seed)  # Sets seed for PyTorch RNG
+    torch.cuda.manual_seed_all(seed)  # Sets seeds of GPU RNG
+    np.random.seed(seed=seed)  # Set seed for NumPy RNG
+    random.seed(seed)  # Set seed for random RNG
+
+set_seeds(2)
+
+retention_time = 3
+agent = Agent(retention_time)
+batch_size = 1
+rewards = []
+avg_rewards = []
+epsilon = 0.1
+gamma=0.99
+tau=1e-2
+
+T_MAX = 300
 
 
-class EpisodeSelector(Dataset):
+buffer_data = pd.read_csv("./data/buffer_data.csv")
+for index, row in buffer_data.iterrows():
+    state = torch.load("./data/states/"+str(row["id"])+".pt")
+    new_state = torch.load("./data/next_states/"+str(row["id"])+".pt")
+    action = [[row["forward_force"], row["right_force"], row["angular"]]]
+    agent.replay_buffer.push(state, torch.tensor(action), row["reward"], new_state, row["done"])
+
+agent.replay_buffer.sampling_ids = np.load("./data/sampling_ids.npy")
+
+
+class my_dataset(Dataset):
     def __init__(self):
-        self.metadata = pd.read_csv("./data/metadata.csv")
+        self.buffer = agent.replay_buffer.buffer
+        self.transform = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+
 
     def __len__(self):
-        return len(self.metadata)
+        return len(agent.replay_buffer.sampling_ids)
+
+    def random_flip(self, current_state_memory, action, next_state_memory):
+        if random.uniform(0, 1) < 0.5:
+            current_state_memory = torch.flip(current_state_memory, dims=[3])
+            next_state_memory = torch.flip(next_state_memory, dims=[3])
+            action = torch.tensor([action[0], -1*action[1], -1*action[2]])
+
+        return current_state_memory, action, next_state_memory
 
     def __getitem__(self, idx):
-        task = self.metadata.iloc[idx]["task"]
-        episode_idx = self.metadata.iloc[idx,1]
+
+        state, action, reward, next_state, done = self.buffer[idx]
+        reward, done = torch.tensor(reward), torch.tensor(done)
+        action = action[0]
+        current_state_memory = state.reshape(state.shape[0], 1, *state.shape[1:])
+        for _id in range(idx-1, idx-retention_time, -1):
+            old_state = self.buffer[_id][0]
+            old_state = old_state.reshape(old_state.shape[0], 1, *old_state.shape[1:])
+
+            current_state_memory = torch.cat((current_state_memory, old_state), dim=1)
         
-        output = pd.read_csv("./data/"+task+"/"+str(episode_idx)+"/output.csv")
-        return task, episode_idx, output
-    
+        next_state = next_state.reshape(next_state.shape[0], 1, *next_state.shape[1:])
+        next_state_memory = torch.cat((next_state, current_state_memory[:,:-1]), dim=1)
 
-episode_selector = EpisodeSelector()
-lr = 1e-2
+        current_state_memory = self.transform(current_state_memory.permute(1,0,2,3)).permute(1,0,2,3)
+        next_state_memory = self.transform(next_state_memory.permute(1,0,2,3)).permute(1,0,2,3)
 
-brain_optimizer = torch.optim.Adam(agent.get_brain_parameters(), lr=lr*1e-2, 
-                    betas=(0.92, 0.999))
-loss_fn = nn.MSELoss()
+        #Augmentation
+        current_state_memory, action, next_state_memory = self.random_flip(current_state_memory, action, next_state_memory)
+        return current_state_memory, action, reward, next_state_memory, done
+
+
+dataset = my_dataset()
+
+train_size = int(0.8 * len(dataset))
+test_size = len(dataset) - train_size
+train_dataset, test_dataset = torch.utils.data.random_split(dataset, [train_size, test_size])
+
+train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+test_dataloader = DataLoader(test_dataset, batch_size=batch_size, shuffle=True)
+
+
+device1 = torch.device("cuda:0") # critic
+device2 = torch.device("cuda:1") # actor
+
 
 def train_loop(epoch):
-    size = len(episode_selector)
-    for batch, (task, episode_idx, label_df) in enumerate(episode_selector):
+    size = len(train_dataloader.dataset)
+    total_critic_loss, total_actor_loss = 0, 0
+    for batch, (states, actions, rewards, next_states, done_batch) in enumerate(train_dataloader):
+        states_device1 = states.to(device1)
+        states_device2 = states.to(device2)
 
-        agent.attention_model.prev_Q = torch.zeros(16, 256, 16, 16).to(device) 
+        actions_device1 = actions.to(device1)
+        
+        next_states_device1 = next_states.to(device1)
+        next_states_device2 = next_states.to(device2)
+        
 
-        task_memory_optimizer = torch.optim.Adam(agent.get_task_memory_parameters(task), lr=lr, 
-                            betas=(0.92, 0.999))
+        agent.actor_target.eval()
+        agent.critic_target.eval()
+        agent.critic.eval()
+        # Critic loss
+        Qvals = agent.critic(states_device1, actions_device1).squeeze()
+        next_actions = agent.actor_target(next_states_device2).to(device1)
+        next_Q = agent.critic_target(next_states_device1, next_actions.detach()).squeeze().cpu()
+        Qprime = (rewards + gamma * next_Q * done_batch).to(device1)
 
-        loss = 0
-        for index, row in label_df.iterrows():
-            observation = torchvision.io.read_image("./data/"+task+"/"+str(episode_idx)+"/"+str(int(row["id"]))+".png").float().to(device)
+        agent.critic.train()
+        agent.critic_optimizer.zero_grad()
+        critic_loss = agent.critic_criterion(Qvals.float(), Qprime.float())
+        critic_loss.backward(retain_graph=True, inputs=list(agent.critic.parameters())) 
+        agent.critic_optimizer.step()
 
-            pred_action_dist, pred_value = agent(observation, task)
 
-            action_dist = torch.tensor([row["forward_force_mean"], 5, row["right_force_mean"], 5, row["angular_velocity_mean"], 5]).to(device)
-            value = torch.tensor([row["value"]]).to(device)
+        agent.critic.eval()
+        agent.actor_optimizer.zero_grad()
+        # Actor loss
+        policy_loss = -agent.critic(states_device1, agent.actor(states_device2).to(device1)).mean()
+        policy_loss.backward(inputs=list(agent.actor.parameters()))
+        agent.actor_optimizer.step()
 
-            loss += loss_fn(pred_action_dist.float(), action_dist.float())
-            loss += loss_fn(pred_value.float(), value.float())
 
-        writer.add_scalar('training loss',
-            loss / 1000,
-            batch + size*epoch)
+        total_actor_loss += policy_loss.detach()
+        total_critic_loss += critic_loss.detach()
 
-        brain_optimizer.zero_grad()
-        task_memory_optimizer.zero_grad()
 
-        loss.backward()
+        # update target networks 
+        for target_param, param in zip(agent.actor_target.parameters(), agent.actor.parameters()):
+            target_param.data.copy_(param.data * tau + target_param.data * (1.0 - tau))
+       
+        for target_param, param in zip(agent.critic_target.parameters(), agent.critic.parameters()):
+            target_param.data.copy_(param.data * tau + target_param.data * (1.0 - tau))
 
-        brain_optimizer.step()
-        task_memory_optimizer.step()
 
-        #if batch % 100 == 0:
-        loss, current = loss.item(), batch
-        print(f"loss: {loss:>7f}  [Epoch: {epoch}; {current:>5d}/{size:>5d}]")
+        if batch % 20 == 0:
+            current = batch * len(done_batch)
+            print(f"Epoch {epoch}; actor loss: {policy_loss:>7f}, critic loss: {critic_loss:>7f}  [{current:>5d}/{size:>5d}]")
+            
+    writer.add_scalar('critic loss',
+        total_critic_loss,
+        epoch)
+    writer.add_scalar('actor loss',
+        total_actor_loss,
+        epoch)
+    torch.save(agent.actor.state_dict(), "./parameters/actor")
+    torch.save(agent.critic.state_dict(), "./parameters/critic")
 
-epochs = 200
+
+
+epochs = 100
 for epoch in range(epochs):
     train_loop(epoch)
-    agent.save_parameters()
-
-
-
-

utils.py
@@ -48,14 +48,15 @@ class ReplayBuffer:
         experience = (state, action, reward, next_state, done)
         self.buffer.append(experience)
 
-    def sample(self, batch_size, retention_time):
+    def sample(self, batch_size, retention_time, batch_ids=None):
         state_batch = []
         action_batch = []
         reward_batch = []
         next_state_batch = []
         done_batch = []
 
-        batch_ids = np.random.choice(self.sampling_ids, batch_size)
+        if batch_ids is None:
+            batch_ids = np.random.choice(self.sampling_ids, batch_size)
 
         for id in batch_ids:
   